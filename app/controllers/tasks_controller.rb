f21834f3fe320fad24fdc4eeedf32065ca22413e
shu.yamaguchi <pork9016@gmail.com>
2018-11-25 07:57:13 +0000

    a

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy] 
-  before_action :correct_user, only: [:destroy  ]
+  before_action :correct_user, only: [:destroy　, :show , :update  ]
+  
 
  
   def index
